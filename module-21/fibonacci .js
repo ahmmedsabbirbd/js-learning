3252fff3a24ae1058c0eedfd38a29ccdea9551a7
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-11-02 12:41:15 +0600

    unexpected input return

--- module-21/fibonacci .js
+++ module-21/fibonacci .js
@@ -1,9 +1,16 @@
 /*** Fibonacci function */
-function fibo(number) {
-    for(let i = 2; i <= 10; i++) {
+function fibo(number, nth) {
+    if(typeof(nth) !== 'number') {
+    }
+    
+    if(nth < 2 ) {
+        return 'Please give a 2 & 2+ number';
+    }
+
+    for(let i = 2; i < nth; i++) {
         number[i] = number[i - 1] + number[i - 2];
     }
     return number;
 }
 
-console.log(fibo([1, 2]));
+console.log(fibo([1, 2], 2));
