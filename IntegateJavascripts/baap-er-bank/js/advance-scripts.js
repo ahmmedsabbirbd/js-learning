fbcd3f61172d8a4f24945b39b3de2592f538d0df
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-11-10 23:10:27 +0600

    complete module 27 and you want to sort code then you can work in next day

--- IntegateJavascripts/baap-er-bank/js/advance-scripts.js
+++ IntegateJavascripts/baap-er-bank/js/advance-scripts.js
@@ -15,6 +15,7 @@ if (submitButton) {
     });
 }
 
+// get Amonunt
 function getInput (input, balanceDeWi) {
     const inputNumber = document.getElementById(input);
     const balance = document.getElementById(balanceDeWi);
@@ -28,20 +29,71 @@ function getInput (input, balanceDeWi) {
     return [tempuraryBalanceNumber, totalBalance];
 }
 
-// handle deposit method 
-document.getElementById('deposit-button').addEventListener('click', function(){
-    const [tempuraryDepositBalanceNumber, totalBalance] = getInput('deposit-input', 'deposit-balance');
-    
-    // Deposit Balance + Total Balance 
+// inputUpdate Amonunt
+function inputUpdate (tempuraryDepositBalanceNumber, totalBalance) {
     let totalBalanceNumber = parseFloat(totalBalance.innerText);
     totalBalance.innerText = totalBalanceNumber += tempuraryDepositBalanceNumber;
+}
+
+// modal
+function modal() {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    const overly = document.createElement('div');
+    overly.classList.add('overly');
+
+    modal.innerHTML = `
+        <div style='color: red'>Plese add valid amount</div>
+        <button id="close-button">Close Modal</button>
+    `;
+
+    document.body.appendChild(modal);
+    document.body.prepend(overly);
+    document.body.style.overflow = 'hidden';
+    
+    const closeButton = document.getElementById('close-button');
+    closeButton.addEventListener('click', function(e){
+        document.body.removeChild(e.target.parentNode);
+        document.body.removeChild(overly);
+        document.body.style.overflowX = 'hidden';
+    });
+}
+
+const inputs = document.getElementsByTagName('input');
+
+for(const input of inputs) {
+    input.addEventListener('keyup', function(){
+        if(!(input.value)) {
+            document.getElementById('withdraw-button').setAttribute('disabled', true);
+            document.getElementById('deposit-button').setAttribute('disabled', true);
+        } else {
+            document.getElementById('withdraw-button').removeAttribute('disabled', true);
+            document.getElementById('deposit-button').removeAttribute('disabled');
+        }
+    })
+}
+
+// handle deposit method 
+document.getElementById('deposit-button').addEventListener('click', function(){
+
+    if( document.getElementById('deposit-input').value > 0) {
+        const [tempuraryDepositBalanceNumber, totalBalance] = getInput('deposit-input', 'deposit-balance');
+        // Deposit Balance + Total Balance 
+        inputUpdate (tempuraryDepositBalanceNumber, totalBalance);
+    } else { 
+        modal();
+        document.getElementById('deposit-input').value = '';
+    }
 })
 
 // handle withdraw method 
 document.getElementById('withdraw-button').addEventListener('click', function(){
-    const [tempuraryWithdrawBalanceNumber, totalBalance] = getInput('withdraw-input', 'withdraw-balance');
-    
-    // Withdraw Balance - Total Balance  
-    let totalBalanceNumber = parseFloat(totalBalance.innerText);
-    totalBalance.innerText = totalBalanceNumber -= tempuraryWithdrawBalanceNumber;
+    if(document.getElementById('withdraw-input').value > 0 && document.getElementById('withdraw-input').value <= parseFloat(document.getElementById('total-balance').innerText)) {
+        const [tempuraryWithdrawBalanceNumber, totalBalance] = getInput('withdraw-input', 'withdraw-balance');
+        // Withdraw Balance - Total Balance  
+        inputUpdate (-(tempuraryWithdrawBalanceNumber), totalBalance);
+    } else {
+        modal();
+        document.getElementById('withdraw-input').value = '';
+    }
 })
