5002d6819668880c31ba7fedd65e1532d2f9312c
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-11-13 20:53:42 +0600

    search with indexOf and includes and startswith

---
+++ IntegateJavascripts/module-29/case-tolowercase-touppercase.js
@@ -0,0 +1,10 @@
+const song = 'amar sonar bangla ami tumai valobashi';
+
+const search = 'sonar';
+
+const userInput = 'blackDiamonD';
+const savedUserInput = 'blackDiamond';
+
+if(userInput.toLowerCase() == savedUserInput.toLowerCase()) {
+    console.log('saved');
+}
---
+++ IntegateJavascripts/module-29/search.js
@@ -0,0 +1,56 @@
+// Method 1 indexOf()
+const products = [
+    'vivo 4gb 64gb Led ',
+    'symphony 1gb 8gb led lower bugget',
+    'laptop 12gb',
+];
+
+const searching = 'lEd';
+
+let output = [];
+
+for(product of products) {
+    if(product.toLowerCase().indexOf(searching.toLowerCase()) != -1) {
+        output.push(product);
+    }
+}
+
+// console.log(output);
+
+// Method 2 includes()
+const products1 = [
+    'vivo 4gb 64gb Led ',
+    'symphony 1gb 8gb led lower bugget',
+    'laptop 12gb symphony',
+];
+
+const searching1 = 'lEd';
+
+let output1 = [];
+
+for(product of products1) {
+    if(product.toLowerCase().includes(searching1.toLowerCase())) {
+        output1.push(product);
+    }
+}
+
+// console.log(output1);
+
+// Method 3 startsWith()
+const products2 = [
+    'vivo 4gb 64gb Led ',
+    'dell symphony  1gb 8gb led lower bugget',
+    'dell laptop 12gb symphony',
+];
+
+const searching2 = 'Dell';
+
+let output2 = [];
+
+for(product of products2) {
+    if(product.toLowerCase().startsWith(searching2.toLowerCase())) {
+        output2.push(product); 
+    }
+}
+
+console.log(output2);
