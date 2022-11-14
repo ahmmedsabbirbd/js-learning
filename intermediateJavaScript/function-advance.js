d12273b969cd6f2281f9ba5e6856862850b44d8a
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-11-14 15:39:40 +0600

    31-8 working

---
+++ intermediateJavaScript/function-advance.js
@@ -0,0 +1,14 @@
+// function exprestion anonimas 
+const advanceFuntion = function (a, b) {
+    return a + b;
+}
+
+// arrow function
+const advanceFuntion1 = (a, b) => a + b;
+
+// arrow function
+const advanceFuntion2 = (a, b) => {
+    return a + b;
+};
+
+console.log(advanceFuntion1(1, 3))
---
+++ intermediateJavaScript/template-string.js
@@ -0,0 +1,11 @@
+const name = 'sabbir';
+const song = 'amar sonar bangla \n' + 'ami tumai valobashi';
+
+const love = 'oishi';
+const newSong = `amar sonar bangla
+ami ${love} valobashi
+`;
+
+const string = `${love} ${love}`
+
+console.log(string);
