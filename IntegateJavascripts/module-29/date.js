0b3d002e3356c042fd063de8b3b55550f0898a90
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-11-14 14:32:16 +0600

    milestone 5 complete

---
+++ IntegateJavascripts/module-29/date.js
@@ -0,0 +1,4 @@
+const date = new Date();
+const faDate = new Date(2002, 11, 27,22,22,22,22);
+
+console.log(faDate);
---
+++ IntegateJavascripts/module-29/try-catch.js
@@ -0,0 +1,20 @@
+// // const name = 'name';
+// // name = 'tumi';
+
+// // for(let i = 0; i > 10 i++){
+    
+// // }
+
+// const object = undefined;
+
+// console.log(object.name)
+
+const myName = 'sabbr';
+
+try {
+    myName = 'jan';
+} catch(er) {
+    console.log('get error', er);
+}
+
+console.log(myName);
