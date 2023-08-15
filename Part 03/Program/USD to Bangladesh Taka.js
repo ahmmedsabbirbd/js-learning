c5db7010a176d334c67182a21cb83ec7cc084fa9
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2023-08-15 09:39:13 +0600

    master branch clear

--- Part 03/Program/USD to Bangladesh Taka.js
+++
@@ -1,5 +0,0 @@
-var one_dollar = 80;
-var my_dollar = 25;
-var my_tk = one_dollar * my_dollar;
-
-console.log("US Dollar", my_dollar, "conver Taka", my_tk, "tk");
--- Part 04/part 4.10/program of 4.10.js
+++
@@ -1,8 +0,0 @@
-/// Program of 4.10 and It's Ternary operators
-console.log("Program of Part 4.10");
-
-var name = "Sabbir Shafi";
-var name_checker;
-
-name_checker = ("Sabbir Ahmmed" == name) ? ". And he's student of Fulbari Model High School" : ". And It's not resistered. By Fulbari Model High School";
-console.log("This name is ", name , name_checker)
